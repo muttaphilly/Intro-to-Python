95ac6d755b1246ab4d64c7b9d4760707e4163bee
Philip Curry <119086803+muttaphilly@users.noreply.github.com>
2023-06-20 09:49:11 +0800

    Console print, tests & pylint

---
+++ Intro/hello-world/hello.py
@@ -0,0 +1,9 @@
+# pylint: disable=missing-docstring
+
+# Here is what you need to do:
+#   1. Write some code below
+#   2. Save to disk with Ctrl + S (Windows) or Cmd + S (Mac)
+#   3. Run the code
+
+# So it begins
+print("Hello, World!")
